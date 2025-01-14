aaabf6b37687fbd8e859ecfbde180284b4db23c2
Dylan Vu <datvt1@vmogroup.com>
2025-01-14 15:27:20 +0700

    first init

---
+++ src/main/java/com/concourse/concourse_demo/ConcourseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.concourse.concourse_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcourseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcourseDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concourse/concourse_demo/ConcourseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concourse.concourse_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcourseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
